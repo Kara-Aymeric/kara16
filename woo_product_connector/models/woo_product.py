3fd59a5eab2d61eee5f58d00f6e5a650c89071c0
HNK86 <kevin.hennion@outlook.fr>
2023-05-12 15:11:10 +0200

    [IMP] - Management product weight config

--- woo_product_connector/models/woo_product.py
+++ woo_product_connector/models/woo_product.py
@@ -33,9 +33,28 @@ class WooProductWeight(models.Model):
     _description = "WooCommerce product weight"
 
     name = fields.Char(
-        string="Name"
+        string="Name",
+        compute="_compute_name",
+        store=True
+    )
+    quantity = fields.Integer(
+        string="Quantity"
+    )
+    weight_uom = fields.Many2one(
+        'uom.uom',
+        string="Unit of measurement"
     )
     active = fields.Boolean(
         default=True,
         help="By unchecking the active field, you can hide a weight without deleting it."
     )
+
+    @api.depends('quantity', 'weight_uom')
+    def _compute_name(self):
+        """ Compute name """
+        for record in self:
+            name = ""
+            if record.quantity and record.weight_uom:
+                name = f"{record.quantity}{record.weight_uom.name}"
+
+            record.name = name
