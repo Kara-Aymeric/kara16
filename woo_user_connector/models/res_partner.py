71ef1049907fedfb6213159d45e0cd9e686fd1e7
HNK86 <kevin.hennion@outlook.fr>
2023-07-13 16:44:32 +0200

    [IMP] - Get commission config into user profile + [IMP] - ValidationError if commission or discount commission <= 0

--- woo_user_connector/models/res_partner.py
+++ woo_user_connector/models/res_partner.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
@@ -7,7 +8,7 @@ class ResPartner(models.Model):
 
     agent_commission = fields.Float(
         string="Commission",
-        tracking=True
+        tracking=True,
     )
 
     agent_discount_commission = fields.Float(
@@ -18,3 +19,18 @@ class ResPartner(models.Model):
     allow_flexible_payment = fields.Boolean(
         string="Allow flexible payment"
     )
+
+    @api.constrains('agent_commission', 'agent_discount_commission')
+    def _check_agent_reference(self):
+        """ Agent commission must be greater than 0. """
+        for partner in self:
+            if partner.agent_commission <= 0 or partner.agent_discount_commission <= 0:
+                raise ValidationError(_("Agent commission must be greater than 0."))
+
+    @api.onchange('user_id')
+    def _onchange_user_id(self):
+        if self.user_id:
+            self.write({
+                'agent_commission': self.user_id.agent_commission,
+                'agent_discount_commission': self.user_id.agent_discount_commission,
+            })
