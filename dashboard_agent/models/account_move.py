82b1460e41f76c75526742b89cf2300ec3de5c85
HNK86 <kevin.hennion@outlook.fr>
2023-07-17 17:05:12 +0200

    [IMP] - Management button confirm

--- dashboard_agent/models/account_move.py
+++ dashboard_agent/models/account_move.py
@@ -5,6 +5,12 @@ from odoo import api, fields, models, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    def _default_dashboard_agent(self):
+        """ Get value dashboard agent by default """
+        return self.env.context.get('dashboard_agent', False)
+
+    dashboard_agent = fields.Boolean(string="Dashboard agent", default=_default_dashboard_agent)
+
     def action_confirm_payment_for_agent(self):
         """ Validates the payment to change the status of the agent commission """
         print("OK")
