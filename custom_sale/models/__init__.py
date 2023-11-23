9a9b8c7d47464c1c8062d5355a2c5c3a9cbf3c90
HNK86 <kevin.hennion@outlook.fr>
2023-11-23 09:00:32 +0100

    [IMP] - Add agent profile field

--- custom_sale/models/__init__.py
+++ custom_sale/models/__init__.py
@@ -6,6 +6,7 @@ from . import product
 from . import report_element_position
 from . import res_company
 from . import res_config_settings
+from . import res_users
 from . import sale_order
 from . import sale_order_line
 from . import sign
