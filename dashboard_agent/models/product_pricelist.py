cc367e9434372f4cc50b7e98ad2c3b3fb4cc8da0
HNK86 <kevin.hennion@outlook.fr>
2023-12-13 14:05:03 +0100

    [IMP] - access agents on pricelist

--- dashboard_agent/models/product_pricelist.py
+++ dashboard_agent/models/product_pricelist.py
@@ -5,4 +5,8 @@ from odoo import api, fields, models, _
 class ProductPricelist(models.Model):
     _inherit = 'product.pricelist'
 
+    def _get_default_agent_ids(self):
+        return self.env['res.users'].search([]).ids
+
     used_default_agent = fields.Boolean(string="Used by default for agent")
+    access_agent_ids = fields.Many2many('res.users', string="Agent access", default=_get_default_agent_ids)
