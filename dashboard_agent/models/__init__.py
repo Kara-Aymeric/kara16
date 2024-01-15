5b8c9dfa0dde1457b965d15187b4bc2c112689f4
HNK86 <kevin.hennion@outlook.fr>
2024-01-15 10:30:37 +0100

    [IMP] - Rule on products for agents

--- dashboard_agent/models/__init__.py
+++ dashboard_agent/models/__init__.py
@@ -5,5 +5,6 @@ from . import product_pricelist
 from . import relation_agent
 from . import res_config_settings
 from . import res_partner
+from . import res_users
 from . import sale_order
 from . import sale_order_line
--- dashboard_agent/models/product_pricelist.py
+++ dashboard_agent/models/product_pricelist.py
@@ -10,3 +10,20 @@ class ProductPricelist(models.Model):
 
     used_default_agent = fields.Boolean(string="Used by default for agent")
     access_agent_ids = fields.Many2many('res.users', string="Agent access", default=_get_default_agent_ids)
+
+    @api.model
+    def create(self, vals):
+        """ Surcharge create method """
+        record = super(ProductPricelist, self).create(vals)
+        for user in self.env['res.users'].search([]):
+            user.sudo().action_update_access_products()
+
+        return record
+
+    def write(self, vals):
+        """ Surcharge write method """
+        res = super(ProductPricelist, self).write(vals)
+        for user in self.env['res.users'].search([]):
+            user.sudo().action_update_access_products()
+
+        return res
