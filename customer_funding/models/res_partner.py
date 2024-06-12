49cb096dd6fc0bbde432274cdc754139afe1f641
HNK86 <kevin.hennion@outlook.fr>
2024-06-12 22:00:38 +0200

    [IMP] - Assign funding auto on partner profile

--- customer_funding/models/res_partner.py
+++ customer_funding/models/res_partner.py
@@ -10,3 +10,21 @@ class ResPartner(models.Model):
         'partner_id',
         string="Credits customer"
     )
+
+    def assign_customer_funding_auto(self):
+        """ Assign customer funding auto """
+        partner_financier_ids = self.env['partner.financier'].search([('add_auto_partner', '=', True)])
+        for financier in partner_financier_ids:
+            if financier.partner_id:
+                self.env['credit.customer'].create({
+                    'partner_id': self.id,
+                    'partner_financier_id': financier.id,
+                })
+
+    @api.model
+    def create(self, vals):
+        """ Surcharge create method """
+        record = super(ResPartner, self).create(vals)
+        record.assign_customer_funding_auto()
+
+        return record
