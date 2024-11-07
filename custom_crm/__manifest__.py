67e18127ba20dd03fdc95425fc2e77f1a8f581b1
Kévin HENNION <kevin.hennion@outlook.fr>
2024-11-07 10:47:02 +0100

    [IMP] - Update CRM

--- custom_crm/__manifest__.py
+++ custom_crm/__manifest__.py
@@ -9,12 +9,14 @@
     'version': '16.0.24.11.06',
     'license': "LGPL-3",
     'installable': True,
-    'depends': ['crm', 'product'],
+    'depends': ['base', 'contacts', 'crm', 'product'],
     'data': [
         # Security
         'security/ir.model.access.csv',
 
         # Views
         'views/crm_lead_views.xml',
+        'views/partner_typology_views.xml',
+        'views/res_partner_views.xml',
     ],
 }
